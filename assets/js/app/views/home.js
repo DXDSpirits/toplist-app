eb5a1f39d0a4e99428bf87dae80fa2f6cb700305
Xindong Ding <dxd.spirits@gmail.com>
2014-04-04 18:28:37 +0800

    remove carousel on the homepage, one topic is randomly picked.

--- assets/js/app/views/home.js
+++ assets/js/app/views/home.js
@@ -1,65 +1,75 @@
 $(function() {
-    var TopicsView = TopApp.CollectionView.extend({
-        ModelView: TopApp.ModelView.extend({
-            events: { 'click .pk-item': 'pk' },
-            template: TPL['one-topic-page'],
-            className: 'one-topic',
-            render: function() {
-                var attrs = this.model ? this.model.toJSON() : {};
+    
+    var OneTopic = function() {
+        this.topics = new TopApp.Collections.Topics();
+        this.i = 0;
+        this.topics_json = null;
+    };
+    
+    OneTopic.prototype.pick = function(callback) {
+        if (this.topics_json && this.i < this.topics_json.length) {
+            callback && callback(this.topics_json[(this.i++)]);
+        } else {
+            var self = this;
+            var method = this.topics_json ? 'fetchNext' : 'fetch';
+            this.topics[method]({
+                reset: true,
+                success: function(collection) {
+                    self.topics_json = _.shuffle(collection.toJSON());
+                    self.i = 0;
+                    self.pick(callback);
+                }
+            });
+        }
+    };
+    
+    var oneTopic = new OneTopic();
+    
+    var TopicsView = TopApp.View.extend({
+        events: { 'click .pk-item': 'pk' },
+        template: TPL['one-topic-page'],
+        initView: function() {
+            _.bindAll(this, 'render');
+            this.$el.on('webkitAnimationEnd', function(e) {
+                if (e.originalEvent.animationName == "flip") {
+                    $(this).removeClass('animate');
+                }
+            });
+        },
+        render: function(attrs) {
+            this.$el.addClass('animate');
+            var self = this;
+            setTimeout(function() {
                 attrs.image = attrs.candidates[0].image;
                 _shuffle = _.shuffle(attrs.candidates);
                 attrs.avatar = _shuffle[0];
                 attrs.pk1 = _shuffle[1];
                 attrs.pk2 = _shuffle[2];
-                return this.renderTemplate(attrs);
-            },
-            pk: function(e) {
-                var $pkItem = $(e.currentTarget);
-                $pkItem.removeClass('fail').addClass('win')
-                       .siblings().removeClass('win').addClass('fail');
-            }
-        })
+                self.renderTemplate(attrs);
+            }, 250);
+            return this;
+        },
+        pk: function(e) {
+            var $pkItem = $(e.currentTarget);
+            $pkItem.removeClass('fail').addClass('win')
+                   .siblings().removeClass('win').addClass('fail');
+        }
     });
     
     TopApp.Pages.Home = new (TopApp.PageView.extend({
-        events: {},
+        events: {
+            'click .header-btn-right': 'renderTopic'
+        },
         initPage: function() {
             this.views = {
-                topics: new TopicsView({
-                    collection: TopApp.Data.Topics,
-                    el: this.$('.carousel-inner')
-                })
+                topic: new TopicsView({ el: this.$('.one-topic') })
             };
         },
-        fixTopicWidth: function() {
-            var screenWidth = this.$('.wrapper').innerWidth();
-            this.$('.carousel-inner').css('width', screenWidth * TopApp.Data.Topics.length);
-            this.$('.one-topic').css('width', screenWidth);
-        },
-        initScroller: function() {
-            if (!this.scroller) {
-                this.scroller = new IScroll(this.$('.carousel').selector, {
-                    scrollX: true,
-                    scrollY: false,
-                    disableMouse: false,
-                    snap: true,
-                    momentum: false,
-                    eventPassthrough: true
-                });
-            } else {
-                this.scroller.refresh();
-            }
+        renderTopic: function() {
+            oneTopic.pick(this.views.topic.render);
         },
         render: function() {
-            var self = this;
-            TopApp.Data.Topics.fetch({
-                reset: true,
-                success: function() {
-                    self.fixTopicWidth();
-                    self.initScroller();
-                }
-            });
-            
+            this.renderTopic();
         }
     }))({el: $("#view-home")});
 });
